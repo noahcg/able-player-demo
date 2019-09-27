b9be5b561e30fc58a8fbb871e6b89f0deae4b343
nglushien17 <nglushien@ushmm.org>
2019-09-27 15:43:43 -0400

    set up routes and views for different player types

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <video
-      id="video1"
-      data-able-player
-      preload="auto"
-      width="800"
-      height="600"
-      data-youtube-id="RnuGFhVkIlQ"
-    ></video>
+    <header class="navbar">
+      <span class="site-name">Able Player Demo</span>
+      <div class="links">
+        <nav class="nav-links">
+          <div class="nav-item">
+            <router-link class="nav-link" to="/">YouTube Player</router-link>
+          </div>
+          <div class="nav-item">
+            <router-link class="nav-link" to="/simpleplayer">Simple Player</router-link>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
@@ -24,6 +32,73 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.navbar {
+  z-index: 20;
+  right: 0;
+  height: 3.6rem;
+  background-color: #fff;
+  box-sizing: border-box;
+  border-bottom: 1px solid #eaecef;
+  padding: 0.7rem 1.5rem;
+  line-height: 2.2rem;
+  margin-bottom: 2em;
+}
+.navbar .site-name {
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: #2c3e50;
+  position: relative;
+  text-align: left;
+  display: block;
+}
+.navbar .links {
+  padding-left: 1.5rem;
+  box-sizing: border-box;
+  background-color: #fff;
+  white-space: nowrap;
+  font-size: 0.9rem;
+  position: absolute;
+  right: 1.5rem;
+  top: 0.7rem;
+  display: flex;
+}
+.navbar .links .nav-links {
+  display: inline-block;
+  flex: 1;
+}
+.nav-links .nav-item {
+  position: relative;
+  display: inline-block;
+  margin-left: 1.5rem;
+  line-height: 2rem;
+
+  &:first-child {
+    margin-left: 0;
+  }
+}
+a {
+  color: #3eaf7c;
+  text-decoration: none;
+}
+.nav-links a {
+  line-height: 1.4rem;
+  color: inherit;
+}
+.navbar a {
+  display: inline-block;
+}
+.nav-links a.router-link-active,
+.nav-links a:hover {
+  color: #3eaf7c;
+}
+.nav-links a.router-link-active,
+.nav-links a:hover {
+  color: #2c3e50;
+}
+.nav-item > a:not(.external).router-link-active,
+.nav-item > a:not(.external):hover {
+  margin-bottom: -2px;
+  border-bottom: 2px solid #46bd87;
 }
 </style>
---
+++ src/views/SimplePlayer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="simple-player">
+    <video id="video1" data-able-player preload="auto" width="800" height="600">
+      <source
+        type="video/mp4"
+        src="https://exhibitions.ushmm.org/storage/388/df5d493c-3dca-11e8-94fe-0a8fdb86ecba.m4v"
+      />
+    </video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "simpleplayer",
+  mounted() {
+    if (localStorage.getItem('reloaded')) {
+        // The page was just reloaded. Clear the value from local storage
+        // so that it will reload the next time this page is visited.
+        localStorage.removeItem('reloaded');
+    } else {
+        // Set a flag so that we know not to reload the page twice.
+        localStorage.setItem('reloaded', '1');
+        location.reload();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
