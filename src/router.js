c270b9d9c8dcc09e3b037d22d2d56911087a51b9
nglushien17 <nglushien@ushmm.org>
2019-10-01 13:21:08 -0400

    updated the routes

--- src/router.js
+++ src/router.js
@@ -1,23 +1,36 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Home from "./views/Home.vue";
 import YoutubePlayer from "./views/YoutubePlayer.vue";
-import SimplePlayer from "./views/SimplePlayer.vue";
+import VideoPlayer from "./views/VideoPlayer.vue";
+import AudioPlayer from "./views/AudioPlayer.vue";
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
+  linkExactActiveClass: "is-active",
   routes: [
     {
       path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/youtubeplayer",
       name: "youtubeplayer",
       component: YoutubePlayer
     },
     {
-      path: "/simpleplayer",
-      name: "simpleplayer",
-      component: SimplePlayer
+      path: "/videoplayer",
+      name: "videoplayer",
+      component: VideoPlayer
+    },
+    {
+      path: "/audioplayer",
+      name: "audioplayer",
+      component: AudioPlayer
     }
   ]
 });
